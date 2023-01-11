d38a393431959452d3d33df6816a37a7b92458a8
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-11 18:48:48 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # blackjack-simplified
+
+## Blackjack Game
+
+In this mini-project you will work with a partner to complete a simple blackjack game.
+
+## Instructions
+
+- Create a simple blackjack game with the following properties:
+
+  - Deals the player a random number between 4 and 21 inclusive.
+
+    - If the player has 21 they win!
+
+  - Deals a random number between 2 and 11 inclusive to the dealer.
+
+  - If the player hits, add a number between 2 and 11.
+
+    - If the player goes over 21 the lose.
+
+    - If the player has 21, stay for them.
+
+    - Repeat until they choose to stay or they bust
+
+  - Once player stays add a number between 2 and 11 to the dealer's hand.
+
+    - If dealer number is less than 17 add another number
+
+    - Repeat until the number is over over 17 but less than 21, or if the dealer goes over 21
+
+    - If dealer goes over 21 they lose.
+
+  - Once dealer stops, and neither player bust, compare each number to 21. Whoever is closer wins!
+
+- Pseudocode the outline of your game before you write any code.
+
+- Create functions if you begin to repeat yourself.
+
+Copyright © Alexandra Batrak 2023
---
+++ index.js
@@ -0,0 +1,83 @@
+// * Simplified BLACKJACK game
+
+// initialise values worth the initial deal of 2 cards to both sides
+let firstDeal = Math.floor(Math.random() * 22) + 4;
+let dealerHand = firstDeal;
+let playerHand = firstDeal;
+
+// deal cards function
+function dealCards(side) {
+  let deal = side + (Math.floor(Math.random() * 12) + 2);
+  return deal;
+}
+
+const win = 'You win!';
+const loose = 'You lose!';
+
+function checkScore() {
+  // if player has 21, they win
+  if (playerHand === 21) {
+    alert(win);
+    return;
+  }
+  // if player is bust, they lose
+  if (playerHand > 21) {
+    alert(lose);
+    return;
+  }
+  // if dealer has 21, they lose
+  else if (dealerHand === 21) {
+    alert('Dealer wins!');
+    return;
+  }
+}
+// create a play function
+function blackJack() {
+  alert('Your score: ' + playerHand);
+  checkScore();
+
+  while (playerHand < 21) {
+    // ok in confirm = hit
+    let option = confirm('Hit or Stay?');
+    if (option) {
+      playerHand = dealCards(playerHand);
+      alert('Your score: ' + playerHand);
+    } else {
+      // check the score and break the loop
+      checkScore();
+      break;
+    }
+  }
+  // check that player has 21
+  if (playerHand > 21) {
+    alert(loose);
+    // if not, continue with dealer
+  } else if (playerHand <= 21) {
+    while (dealerHand < 17) {
+      dealerHand = dealCards(dealerHand);
+    }
+    // dealer has 17-21 and player has less than dealer, they lose
+    if (dealerHand >= 17 && dealerHand <= 21 && playerHand < dealerHand) {
+      alert('Dealer wins!');
+      return;
+    }
+    // if dealer is bust, they win
+    if (dealerHand > 21) {
+      alert(win);
+      return;
+      // if player has less than 21, dealer has less than 21, and player has more than dealer, they win
+    } else if (playerHand < 21 && dealerHand < 21 && playerHand > dealerHand) {
+      alert(win);
+      return;
+    } else {
+      alert(loose);
+      return;
+    }
+  }
+}
+
+// game init
+let play = confirm('Wanna play BlackJack?');
+if (play) {
+  blackJack();
+}
